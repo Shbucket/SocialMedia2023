cea8e103c3987e19bbc60626057d4d2de8f147f8
shoamri joseph <shomarijoseph@gmail.com>
2023-03-17 15:27:35 -0400

    created procted route

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import {
   RouterProvider,
   Route,
   Outlet,
+  Navigate,
 } from "react-router-dom";
-import Navbar from "./components/navbar/navbar";
-import Rightbar from "./components/rightbar/rightbar";
-import Leftbar from "./components/leftbar/leftbar";
-import Home from "./pages/home/home";
-import Profile from "./pages/profile/profile";
+import Navbar from "./components/navbar/Navbar";
+import Rightbar from "./components/rightbar/Rightbar";
+import Leftbar from "./components/leftbar/Leftbar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
+  const currentUser = false;
+
   const Layout = () => {
     return (
       <div>
@@ -26,20 +29,31 @@ function App() {
     );
   };
 
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
   const router = createBrowserRouter([
     {
-        path: "/",
-        element: <Layout />,
-        children: [
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
         {
-            path: "/",
-            element: <Home />,
+          path: "/",
+          element: <Home />,
         },
         {
-            path: "/profile/:id",
-            element: <Profile />,
-        }
-        ]
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
     },
     {
       path: "/login",
