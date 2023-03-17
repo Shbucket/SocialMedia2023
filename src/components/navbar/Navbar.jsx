bca2295a74fc8f697f55dc4fec8cd092a67d74c4
shoamri joseph <shomarijoseph@gmail.com>
2023-03-17 15:39:15 -0400

    imported icons for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,12 @@
 import './navbar.scss';
-
+import HomeIcon from "@mui/icons-material/Home";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import GridViewIcon from "@mui/icons-material/GridView";
+import SearchIcon from "@mui/icons-material/Search";
+import EmailIcon from "@mui/icons-material/Email";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import LightModeIcon from "@mui/icons-material/LightMode";
 
 const Navbar = () => {
   return (
