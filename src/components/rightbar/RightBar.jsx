fc48048d97168d9dbef2515b9a99c1ae24da198c
shoamri joseph <shomarijoseph@gmail.com>
2023-03-18 12:18:32 -0400

    desgined rightbar

--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -1,10 +1,58 @@
-import './rightbar.scss';
-
+import "./rightbar.scss";
 
 const Rightbar = () => {
   return (
-    <div className='rightBar'>rightbar</div>
-  )
-}
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="" alt="" />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="" alt="" />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
 
-export default Rightbar
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="" alt="" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="" alt="" />
+              <div className="online"/>
+                <span>Jane Doe</span>
+      
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Rightbar;
