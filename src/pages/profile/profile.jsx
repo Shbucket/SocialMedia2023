70aac61b808f9b4b2238f533c42c96266a1108ca
shoamri joseph <shomarijoseph@gmail.com>
2023-03-30 12:12:22 -0400

    created profile ui design

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,8 +1,72 @@
-import './profile.scss';
+import "./profile.scss";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts";
 
 export const Profile = () => {
   return (
-    <div className='profile'>profile</div>
-  )
-}
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/15747000/pexels-photo-15747000.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://images.pexels.com/photos/9024329/pexels-photo-9024329.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <PinterestIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Jane Doe</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>USA</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>Shbucket</span>
+              </div>
+            </div>
+            <button>follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+         <Posts />
+      </div>
+     
+    </div>
+  );
+};
 export default Profile;
